761b6784ae83e403fa602eb6caf9ba79fa44bc0d
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2024-12-30 20:05:12 -0800

    input wrapper improved, button wrapper functionality improved, application entry point updated, redux setup complete authentication of users are more secured and few bug fixes

--- Frontend/src/Components/InputBox.jsx
+++ Frontend/src/Components/InputBox.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 
-const InputBox = ({ LabelName, Placeholder, className, Type }) => {
+const InputBox = ({
+  LabelName,
+  Placeholder,
+  className = "",
+  Type = "text",
+  Name,
+  Value,
+  onChange,
+  Required = true,
+}) => {
   return (
     <div className="flex justify-center items-center w-full">
       <div className="py-4 w-full">
         <label
-          htmlFor="input"
+          htmlFor={Name}
           className="block text-sm font-medium text-gray-700 mb-2"
         >
           {LabelName}
         </label>
         <input
-          //   id="input"
+          id={Name}
+          name={Name}
           type={Type}
+          value={Value}
+          onChange={onChange}
           placeholder={Placeholder}
+          required={Required}
           className={`w-full px-4 py-2 text-gray-700 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200 ease-in-out hover:shadow-md ${className}`}
         />
       </div>
--- Frontend/src/Components/ProductCard.jsx
+++ Frontend/src/Components/ProductCard.jsx
@@ -1,23 +1,38 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
-const ProductCard = ({ ProductName, CurrentPrice, Mrp, Rating, Offer }) => {
+const ProductCard = ({
+  ProductName,
+  CurrentPrice,
+  Mrp,
+  Rating,
+  Offer,
+  Description,
+  Image,
+  productId,
+}) => {
   return (
-    <div className="whiteSoftBG shadow-md hover:shadow-lg h-auto w-full sm:w-80 md:w-72 lg:w-80 overflow-hidden rounded-lg hover:scale-105 duration-300 ease-in-out">
-      <div className="text-2xl h-2/3 p-2">No Image Found</div>
-      <div className="h-1/3 px-4 py-3 gap-2 flex flex-col">
-        <h1 className="text-xl sm:text-2xl font-semibold">{ProductName}</h1>
-        <div className="flex w-full justify-start items-center gap-4 sm:gap-10">
-          <h1 className="text-lg sm:text-xl">₹ {CurrentPrice}</h1>
-          <h1 className="text-sm sm:text-base line-through text-gray-500">
-            {Mrp}
-          </h1>
+    <Link to={`/current-product/${productId}`} className="w-fit">
+      <div className="whiteSoftBG shadow-md hover:shadow-lg h-auto w-fit overflow-hidden rounded-lg hover:scale-105 duration-300 ease-in-out">
+        <div className="text-2xl p-2 bg-neutral-200 m-2 w-64 h-52">
+          <img src={Image} alt="No Image Found" />
         </div>
-        <div className="flex justify-start items-center w-full gap-4 sm:gap-10 mt-2">
-          <h1 className="text-sm sm:text-base">{Rating}</h1>
-          <h1 className="text-sm sm:text-base text-green-600">{Offer}</h1>
+        <div className="h-1/3 px-4 py-3 gap-2 flex flex-col">
+          <h1 className="text-xl sm:text-2xl font-semibold">{ProductName}</h1>
+          <div className="flex w-full justify-start items-center gap-4 sm:gap-10">
+            <h1 className="text-lg sm:text-xl">₹ {CurrentPrice}</h1>
+            <h1 className="text-sm sm:text-base line-through text-gray-500">
+              {Mrp}
+            </h1>
+          </div>
+          <div className="flex justify-start items-center w-full gap-4 sm:gap-10 mt-2">
+            <p className="text-sm sm:text-base">{Description}</p>
+            <h1 className="text-sm sm:text-base">{Rating}</h1>
+            <h1 className="text-sm sm:text-base text-green-600">{Offer}</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
