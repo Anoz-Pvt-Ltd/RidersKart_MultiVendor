4e9e592e0d1da8c052798dbbf8d2fc34cdbcb896
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-04-23 20:40:49 +0530

    Add login functionality to BuyNow component; implement user authentication and address selection

--- Frontend/src/Pages/BuyNow/BuyNow.jsx
+++ Frontend/src/Pages/BuyNow/BuyNow.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { FetchData } from "../../Utility/FetchFromApi";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../Components/ProductCard";
 import Button from "../../Components/Button";
 import LoadingUI from "../../Components/Loading";
+import { MoveRight } from "lucide-react";
+import InputBox from "../../Components/InputBox";
 
 const BuyNow = ({ startLoading, stopLoading }) => {
   const { productId } = useParams();
@@ -18,6 +20,7 @@ const BuyNow = ({ startLoading, stopLoading }) => {
   const [addAmount, setAddAmount] = useState("");
 
   const Navigate = useNavigate();
+  const Dispatch = useDispatch();
   const HandleHome = () => {
     Navigate("/");
   };
@@ -100,9 +103,7 @@ const BuyNow = ({ startLoading, stopLoading }) => {
   const handlePaymentChange = (e) => {
     setPaymentMethod(e.target.value);
   };
-  // console.log(user?.[0]?._id);
-  // console.log(product?._id);
-  // console.log(productVendor?._id);
+
   const handleBuyNow = async () => {
     try {
       startLoading();
@@ -180,80 +181,218 @@ const BuyNow = ({ startLoading, stopLoading }) => {
     e.preventDefault();
   };
 
-  return (
-    <div className="buy-now-page flex flex-col lg:flex-row justify-center items-center gap-20 mt-10">
-      {product && (
-        <div className="w-fit">
-          <ProductCard
-            Image={product?.images[0]?.url}
-            ProductName={product?.name}
-            Description={product?.description}
-            productId={product?._id}
-            CurrentPrice={product?.price.sellingPrice}
-            Mrp={product?.price.MRP}
-          />
-        </div>
-      )}
+  const Login = () => {
+    const NavigateRegister = () => {
+      Navigate("/register");
+    };
 
-      <div className="address-selection flex flex-col gap-5 justify-evenly items-center">
-        <div>
-          <h1 className="font-semibold text-2xl">Hello, {user?.[0]?.name}</h1>
-        </div>
-        <div className="w-4/5 ">
-          <h2 className="mb-5 font-semibold">Select Shipping Address</h2>
-          {addresses?.length > 0 ? (
-            <select
-              value={selectedAddress}
-              onChange={handleAddressChange}
-              className="bg-white p-5 rounded-xl outline-none w-full shadow-xl border"
+    const formRef = useRef(null);
+    const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+    });
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError("");
+      setSuccess("");
+      startLoading(); // Start loading when login button is clicked
+
+      try {
+        const response = await FetchData("users/login", "post", formData);
+        console.log(response);
+        localStorage.clear(); // will clear the all the data from localStorage
+        localStorage.setItem(
+          "AccessToken",
+          response.data.data.tokens.AccessToken
+        );
+        localStorage.setItem(
+          "RefreshToken",
+          response.data.data.tokens.RefreshToken
+        );
+
+        alert(response.data.message);
+        Dispatch(clearUser());
+        Dispatch(addUser(response.data.data.user));
+        setSuccess("Login successful!");
+        Navigate(`/checkout/${productId}/${user?._id}`);
+      } catch (err) {
+        console.log(error);
+        // alert(parseErrorMessage(error.response.data.data.statusCode));
+        alert("Invalid login credentials");
+      } finally {
+        stopLoading(); // Stop loading once response is received
+      }
+    };
+
+    return (
+      <div>
+        <section className="flex lg:m-28 lg:border rounded-lg lg:shadow-md lg:shadow-neutral-300 lg:h-96 my-20">
+          <div className="lg:w-1/2 p-10 rounded-lg rounded-r-none headerBg text-white hidden lg:block">
+            <h1 className="text-4xl h-3/4 text-white font-semibold">
+              Login with your e-mail to get started !
+            </h1>
+            <h1 className="flex justify-around items-center">
+              If you are not registered with us create your new account here{" "}
+              <MoveRight />
+            </h1>
+          </div>
+          <div className=" lg:w-1/2 w-full flex justify-center items-center flex-col lg:whiteSoftBG">
+            <form
+              ref={formRef}
+              className="login h-3/4 flex justify-top items-top flex-col w-3/4"
             >
-              <option value="" selected disabled>
-                Select an address
-              </option>
-              {addresses?.map((address, index) => (
-                <option
-                  key={address?._id}
-                  value={index}
-                  className="bg-white max-w-40"
+              <InputBox
+                onChange={handleChange}
+                LabelName={"Login"}
+                Placeholder={"Email Address"}
+                Type={"email"}
+                className={"w-full"}
+                Name={"email"}
+                Value={formData.email}
+              />
+              <InputBox
+                onChange={handleChange}
+                LabelName={"Password"}
+                Placeholder={"Password"}
+                Type={"password"}
+                className={"w-full"}
+                Name={"password"}
+                Value={formData.password}
+              />
+              {/* <InputBox
+              LabelName={"Login"}
+              Placeholder={"Your Mobile Number"}
+              Type={"number"}
+              className={"w-full"}
+            /> */}
+              <Button
+                className={`w-full bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
+                label={"Login"}
+                onClick={handleSubmit}
+              />
+            </form>
+            <div className="w-full h-full justify-center items-center flex flex-col mt-20 lg:mt-0">
+              <Button
+                label={"Register Here"}
+                onClick={NavigateRegister}
+                className={`w-1/2 hover:bg-green-500 bg-white text-blue-600  hover:text-black`}
+              />
+              <p className="text-xs text-neutral-500 text-center">
+                ** By continuing, you agree to our Terms of Use and Privacy
+                Policy.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {user === null || user.length === 0 ? (
+        <div className="">
+          <div className="flex w-full">
+            <section className="login_or_register text-black flex flex-col  items-center justify-center ">
+              <h1 className="w-full text-center text-2xl font-medium font-serif  txt-green  ">
+                You are not Logged in
+              </h1>
+              <h1 className="w-full text-center text-xl font-light font-Caveat  text-black ">
+                Kindly Login Or Register.
+              </h1>
+            </section>
+          </div>
+          <Login />
+        </div>
+      ) : (
+        <div className="buy-now-page flex flex-col lg:flex-row justify-center items-center gap-20 mt-10">
+          {product && (
+            <div className="w-fit">
+              <ProductCard
+                Image={product?.images[0]?.url}
+                ProductName={product?.name}
+                Description={product?.description}
+                productId={product?._id}
+                CurrentPrice={product?.price.sellingPrice}
+                Mrp={product?.price.MRP}
+              />
+            </div>
+          )}
+
+          <div className="address-selection flex flex-col gap-5 justify-evenly items-center">
+            <div>
+              <h1 className="font-semibold text-2xl">
+                Hello, {user?.[0]?.name}
+              </h1>
+            </div>
+            <div className="w-4/5 ">
+              <h2 className="mb-5 font-semibold">Select Shipping Address</h2>
+              {addresses?.length > 0 ? (
+                <select
+                  value={selectedAddress}
+                  onChange={handleAddressChange}
+                  className="bg-white p-5 rounded-xl outline-none w-full shadow-xl border"
                 >
-                  {`${address?.street}, ${address?.city}, ${address?.state}, ${address?.country}, ${address?.postalCode}`}
+                  <option value="" selected disabled>
+                    Select an address
+                  </option>
+                  {addresses?.map((address, index) => (
+                    <option
+                      key={address?._id}
+                      value={index}
+                      className="bg-white max-w-40"
+                    >
+                      {`${address?.street}, ${address?.city}, ${address?.state}, ${address?.country}, ${address?.postalCode}`}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                <p>No addresses available. Please add one in your profile.</p>
+              )}
+            </div>
+            <div className="w-4/5">
+              <h2 className="mb-5 font-semibold">Select Payment Method</h2>
+              <select
+                value={paymentMethod}
+                onChange={handlePaymentChange}
+                className="bg-white p-5 rounded-xl outline-none w-full shadow-xl border"
+              >
+                <option value="" disabled>
+                  Select a payment method
                 </option>
-              ))}
-            </select>
-          ) : (
-            <p>No addresses available. Please add one in your profile.</p>
+                <option value="online">Online</option>
+                <option value="cash">Cash on delivery</option>
+              </select>
+            </div>
+          </div>
+
+          {paymentMethod === "online" && (
+            <Button
+              className={`mt-5 bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
+              onClick={Payment}
+              label={"Proceed for payment"}
+            />
+          )}
+          {paymentMethod === "cash" && (
+            <Button
+              className={` bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
+              onClick={handleBuyNow}
+              Disabled={!selectedAddress || !paymentMethod}
+              label={"Place order"}
+            />
           )}
         </div>
-        <div className="w-4/5">
-          <h2 className="mb-5 font-semibold">Select Payment Method</h2>
-          <select
-            value={paymentMethod}
-            onChange={handlePaymentChange}
-            className="bg-white p-5 rounded-xl outline-none w-full shadow-xl border"
-          >
-            <option value="" disabled>
-              Select a payment method
-            </option>
-            <option value="online">Online</option>
-            <option value="cash">Cash on delivery</option>
-          </select>
-        </div>
-      </div>
-
-      {paymentMethod === "online" && (
-        <Button
-          className={`mt-5 bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
-          onClick={Payment}
-          label={"Proceed for payment"}
-        />
-      )}
-      {paymentMethod === "cash" && (
-        <Button
-          className={` bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
-          onClick={handleBuyNow}
-          Disabled={!selectedAddress || !paymentMethod}
-          label={"Place order"}
-        />
       )}
     </div>
   );
