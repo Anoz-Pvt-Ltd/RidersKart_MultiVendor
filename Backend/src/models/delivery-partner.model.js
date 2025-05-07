2a3bbae309c9ec347a63271c1d131e41b71ef3ac
Vivek Krishan <vivekkrishan440@gmail.com>
2025-05-07 13:03:06 +0530

    feat: Refactor RegisterDriver component to include loading UI and improve image handling

--- Backend/src/models/delivery-partner.model.js
+++ Backend/src/models/delivery-partner.model.js
@@ -23,12 +23,6 @@ const deliveryPartnerSchema = new Schema(
       trim: true,
     },
 
-    email: {
-      type: String,
-      unique: true,
-      trim: true,
-    },
-
     password: {
       type: String,
       required: true,
