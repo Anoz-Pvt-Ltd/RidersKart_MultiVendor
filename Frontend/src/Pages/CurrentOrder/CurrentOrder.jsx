1c84f2611b6c6567d97047448d7005385159ec8b
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-05-17 00:08:10 +0530

    feat: Update CurrentOrder component to improve order data handling and enhance UI layout

--- Frontend/src/Pages/CurrentOrder/CurrentOrder.jsx
+++ Frontend/src/Pages/CurrentOrder/CurrentOrder.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import LoadingUI from "../../Components/Loading";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useSelector } from "react-redux";
 import { FetchData } from "../../Utility/FetchFromApi";
 import Button from "../../Components/Button";
 
 const CurrentOrder = ({ startLoading, stopLoading }) => {
   const { orderId } = useParams();
-  const [order, setOrder] = useState();
-  const [orderProducts, setOrderProducts] = useState([]);
+  const [order, setOrder] = useState([]);
+  const [orderProducts, setOrderProducts] = useState();
   const [error, setError] = useState(null);
   const [shippingAddress, setShippingAddress] = useState([]);
   const user = useSelector((store) => store.UserInfo.user);
-  console.log(user);
+  const Navigate = useNavigate();
+
+  // console.log(orderProducts);
+  console.log(order);
+  // console.log(shippingAddress);
+
   const tableHeaders = [
     "Product Name",
     "Shipping Details",
@@ -32,11 +37,11 @@ const CurrentOrder = ({ startLoading, stopLoading }) => {
             `orders/admin/current-order/${orderId}`,
             "get"
           );
-          console.log(response);
+          // console.log(response);
           if (response.data.success) {
-            setOrder(response.data.data.orders);
-            setOrderProducts(response.data.data.orders.products);
-            setShippingAddress(response.data.data.orders.shippingAddress);
+            setOrder(response.data.data.order);
+            setOrderProducts(response.data.data.order.products);
+            setShippingAddress(response.data.data.order.shippingAddress);
           } else {
             setError("Failed to load orders.");
           }
@@ -51,62 +56,129 @@ const CurrentOrder = ({ startLoading, stopLoading }) => {
     fetchOrder();
   }, [user]);
 
-  return (
-    <div>
-      CurrentOrder ID :{orderId}
-      <div className=" px-10 py-20">
-        <div className="grid grid-cols-5 grid-rows-0 gap-4">
-          {/* user: shipping details */}
-          <div className=" shadow rounded-xl py-2 px-1 flex justify-start items-start flex-col col-span-2 font-semibold">
-            Shipping Details
-            <h1 className="flex flex-col">
-              <span>
-                Street :{" "}
-                <span className="font-light">{shippingAddress.street}</span>
-              </span>
-              <span>
-                City: <span className="font-light">{shippingAddress.city}</span>
-              </span>
-              <span>
-                State:{" "}
-                <span className="font-light">{shippingAddress.state}</span>
-              </span>
-              <span>
-                Postal Code:{" "}
-                <span className="font-light">{shippingAddress.postalCode}</span>
-              </span>
-            </h1>
+  const ProductsCard = ({
+    productId,
+    productName,
+    sellerName,
+    orderAmount,
+    paymentMethod,
+    images,
+  }) => {
+    return (
+      <div className="flex flex-col justify-center items-center bg-white shadow-md rounded-lg p-4 mb-5 mx-5">
+        <div className="flex justify-between items-center w-full">
+          <div className="px-5 py-2 flex flex-col justify-center items-start w-96 gap-5">
+            <h1 className="text-2xl">{productName}</h1>
+            <h2 className="">Seller: {sellerName}</h2>
+            <h3 className="font-medium gap-10 text-xl">
+              <span>₹ {orderAmount}</span>{" "}
+              <span className="text-sm p-2 rounded-xl">{paymentMethod}</span>
+            </h3>
           </div>
-          {/* user: name  */}
-          <div className=" shadow rounded-xl text-xl py-2 px-1 flex justify-start items-center col-span-2 row-span-2 col-start-1 row-start-2 font-semibold">
-            Name: <span className="font-light ml-2">{user[0]?.name}</span>
+          <div className="w-48 h-48 shadow overflow-hidden flex justify-center items-center rounded-lg object-center">
+            <img src={images} className="" />
           </div>
-          {/* user: contact number */}
-          <div className="shadow rounded-xl  py-2 px-1 flex justify-start items-center col-span-2 col-start-1 row-start-4 font-semibold">
-            Phone number:{" "}
-            <span className="font-light ml-2">{user[0]?.phoneNumber}</span>
-          </div>
-
-          {/* product: name */}
-          <div className="ml-10 border border-neutral-400 py-2 px-1 flex justify-start items-center col-span-2 row-span-2 col-start-3 row-start-1">
-            Product name
-          </div>
-          <div className="border border-neutral-400 py-2 px-1 flex justify-start items-center row-span-2 col-start-5 row-start-1">
-            prodduct image
-          </div>
-          <div className="ml-10 border border-neutral-400 py-2 px-1 flex justify-start items-center col-start-3 row-start-3">
-            seller name
-          </div>
-          <div className=" py-2 px-1 flex justify-center items-center col-start-5 row-start-3">
+        </div>
+        <div className="flex justify-center items-center gap-10 py-5">
+          <div className="">
             <Button label={"Raise an issue for this order"} />
           </div>
-          <div className="font-medium  py-2 px-1 flex justify-center items-center col-start-4 row-start-3 shadow rounded-xl gap-10">
-            <span>₹ {order?.totalAmount}</span>{" "}
-            <span className="font-light">{order?.paymentMethod}</span>
+          <div className="">
+            <Button label={"Download Invoice"} />
           </div>
-          <div className="ml-10 border border-neutral-400 py-2 px-1 flex justify-start items-center col-span-3 col-start-3 row-start-4">
-            rating
+          <div className="">
+            <Button
+              label={"Reorder"}
+              onClick={() => {
+                Navigate(`/current-product/${productId}`);
+              }}
+            />
           </div>
+          <div className="">★★★★★</div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className=" px-10 py-10 flex justify-center items-start">
+        <div>
+          <div className="bg-neutral-300 rounded-xl shadow-xl py-5 px-10 flex justify-start items-start flex-col mb-4 text-lg">
+            <h1 className="text-lg font-semibold">Order Details</h1>
+            <h1 className="font-semibold">
+              Id: <span className="font-normal">{order?._id}</span>
+            </h1>
+            <h1 className="font-semibold">
+              Payment Status:{" "}
+              <span className="font-normal">{order?.paymentStatus}</span>
+            </h1>
+            <h1 className="font-semibold">
+              Payment method:{" "}
+              <span className="font-normal">{order?.paymentMethod}</span>
+            </h1>
+            <h1 className="font-semibold">
+              Order status:{" "}
+              <span className="font-normal">{order?.orderStatus}</span>
+            </h1>
+            <h1 className="font-semibold">
+              Order date:{" "}
+              <span className="font-normal">{order?.bookingDate}</span>
+            </h1>
+            <h1 className="font-semibold">
+              Amount: <span className="font-normal">₹{order?.totalAmount}</span>
+            </h1>
+          </div>
+          <div className="grid grid-cols-4 grid-rows-0 gap-4">
+            {/* user: shipping details */}
+            <div className=" shadow rounded-xl py-5 px-10 flex justify-start items-start flex-col col-span-4 font-semibold bg-neutral-300 text-lg">
+              Shipping Details
+              <h1 className="flex flex-col">
+                <span>
+                  Street :{" "}
+                  <span className="font-normal">{shippingAddress.street}</span>
+                </span>
+                <span>
+                  City:{" "}
+                  <span className="font-normal">{shippingAddress.city}</span>
+                </span>
+                <span>
+                  State:{" "}
+                  <span className="font-normal">{shippingAddress.state}</span>
+                </span>
+                <span>
+                  Postal Code:{" "}
+                  <span className="font-normal">
+                    {shippingAddress.postalCode}
+                  </span>
+                </span>
+              </h1>
+            </div>
+            {/* user: name  */}
+            <div className=" shadow rounded-xl text-xl py-5 px-10 flex justify-start items-center col-span-4  col-start-1 row-start-2 font-semibold bg-neutral-300">
+              Name: <span className="font-normal ml-2">{user[0]?.name}</span>
+            </div>
+            {/* user: contact number */}
+            <div className="shadow rounded-xl text-lg py-5 px-10 flex justify-start items-center col-span-4 row-start-4 font-semibold bg-neutral-300">
+              Phone number:{" "}
+              <span className="font-normal ml-2">{user[0]?.phoneNumber}</span>
+            </div>
+
+            {/* product: name */}
+          </div>
+        </div>
+        <div className="">
+          {orderProducts?.map((product, index) => (
+            <ProductsCard
+              key={index}
+              productId={product?.product?._id}
+              productName={product?.product?.name}
+              sellerName={product?.seller?.name}
+              orderAmount={product?.price?.MRP}
+              paymentMethod={order?.paymentMethod}
+              images={product?.product?.images[0]?.url}
+            />
+          ))}
         </div>
       </div>
     </div>
