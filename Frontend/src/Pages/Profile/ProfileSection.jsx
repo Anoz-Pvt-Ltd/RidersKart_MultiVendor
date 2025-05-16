597e19ef09269ebe122f25604a2b2104c5483f38
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-05-17 00:22:21 +0530

    feat: Enhance ProfileSection with improved error handling, modal functionality, and UI updates for address and profile editing

--- Frontend/src/Pages/Profile/ProfileSection.jsx
+++ Frontend/src/Pages/Profile/ProfileSection.jsx
@@ -138,10 +138,14 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
         window.location.reload();
       } else {
         setError("Failed to update profile.");
+        window.location.reload();
       }
     } catch (err) {
-      setError(err.response?.data?.message || "Failed to update profile.");
+      window.location.reload();
+      alert(err.response?.data?.message || "Failed to update profile.");
+      console(err.response?.data?.message || "Failed to update profile.");
     } finally {
+      window.location.reload();
       stopLoading();
     }
   };
@@ -151,7 +155,7 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
     <section className="flex justify-center items-center flex-col">
       <div className="flex lg:flex-row flex-col w-full justify-evenly items-center lg:shadow py-2 rounded-xl">
         {/* Profile_area */}
-        <div className="name">
+        <div className="name flex flex-col gap-2">
           <h2 className="text-2xl font-bold lg:flex justify-center items-center hidden">
             <span>
               <UserCheck className="mr-5" />
@@ -159,18 +163,14 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
             Your Profile
           </h2>
           <p>
-            Name:{" "}
-            <span className="lg:text-2xl font-bold">{user?.[0]?.name}</span>
+            Name: <span className="lg:text-2xl">{user?.[0]?.name}</span>
           </p>
           <p>
-            Email:{" "}
-            <span className="lg:text-2xl font-bold">{user?.[0]?.email}</span>
+            Email: <span className="lg:text-2xl">{user?.[0]?.email}</span>
           </p>
           <p>
             Number:{" "}
-            <span className="lg:text-2xl font-bold">
-              {user?.[0]?.phoneNumber}
-            </span>
+            <span className="lg:text-2xl">{user?.[0]?.phoneNumber}</span>
           </p>
         </div>
         {/* all_FourButtons */}
@@ -242,10 +242,9 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
       {/* address area */}
       <div className="address flex flex-wrap flex-col">
         <p className=" font-bold text-xl lg:hidden block">Address:</p>
-        <p className=" font-bold text-xl hidden lg:block">
-          Select a Default Address:
-        </p>
-        <div className="flex lg:flex-row flex-col">
+        <p className=" font-bold text-xl hidden lg:block">Available address:</p>
+        {/* address list  */}
+        <div className="flex lg:flex-row flex-col flex-wrap justify-start items-center">
           {user?.[0]?.address?.map((address, index) => (
             <div
               key={address._id}
@@ -261,20 +260,28 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
               /> */}
               <span className="shadow m-2 p-4 rounded-xl">
                 <li className="ml-4 font-semibold list-none">
-                  Street: <span className="font-thin ">{address.street}</span>
+                  Street:{" "}
+                  <span className="font-normal text-lg ">{address.street}</span>
                 </li>
                 <li className="ml-4 font-semibold list-none">
-                  City: <span className="font-thin ">{address.city}</span>
+                  City:{" "}
+                  <span className="font-normal text-lg ">{address.city}</span>
                 </li>
                 <li className="ml-4 font-semibold list-none">
-                  Country: <span className="font-thin ">{address.country}</span>
+                  Country:{" "}
+                  <span className="font-normal text-lg ">
+                    {address.country}
+                  </span>
                 </li>
                 <li className="ml-4 font-semibold list-none">
                   Postal Code:{" "}
-                  <span className="font-thin ">{address.postalCode}</span>
+                  <span className="font-normal text-lg ">
+                    {address.postalCode}
+                  </span>
                 </li>
                 <li className="ml-4 font-semibold list-none">
-                  State: <span className="font-thin ">{address.state}</span>
+                  State:{" "}
+                  <span className="font-normal text-lg ">{address.state}</span>
                 </li>
                 <div className="flex justify-center items-center gap-5 pt-5">
                   <button className="flex justify-center items-center gap-2 hover:text-green-500">
@@ -297,140 +304,139 @@ const ProfileSection = ({ startLoading, stopLoading }) => {
             </div>
           ))}
         </div>
-
-        {/* <button onClick={openModal}>Add Address</button> */}
-
-        {showModal && (
-          <div className="modal backdrop-blur-lg fixed top-0 left-0 w-full h-full flex justify-center items-center">
-            <div className="modal-content flex  justify-center items-center gap-20 bg-white p-4 rounded-xl">
-              <span
-                className="close cursor-pointer bg-neutral-300 rounded-full p-2 flex justify-center items-center text-3xl w-fit"
-                onClick={closeModal}
-              >
-                &times;
-              </span>
-              <h3>Add New Address</h3>
-              <form>
-                <InputBox
-                  LabelName="Street"
-                  Placeholder="Enter street address"
-                  Name="street"
-                  Value={newAddress.street}
-                  onChange={handleAddressInputChange}
-                />
-                <InputBox
-                  LabelName="City"
-                  Placeholder="Enter city"
-                  Name="city"
-                  Value={newAddress.city}
-                  onChange={handleAddressInputChange}
-                />
-                <InputBox
-                  LabelName="Country"
-                  Placeholder="Enter country"
-                  Name="country"
-                  Value={newAddress.country}
-                  onChange={handleAddressInputChange}
-                />
-                <InputBox
-                  LabelName="Postal Code"
-                  Placeholder="Enter postal code"
-                  Name="postalCode"
-                  Value={newAddress.postalCode}
-                  onChange={handleAddressInputChange}
-                />
-                <InputBox
-                  LabelName="State"
-                  Placeholder="Enter state"
-                  Name="state"
-                  Value={newAddress.state}
-                  onChange={handleAddressInputChange}
-                />
+      </div>
+      {/* Add address modal */}
+      {showModal && (
+        <div className="modal backdrop-blur-lg bg-black/70 fixed top-0 left-0 w-full h-full flex justify-center items-center">
+          <div className="modal-content flex  justify-center px-20 items-center gap-20 bg-white p-4 rounded-xl">
+            <form>
+              <InputBox
+                LabelName="Street"
+                Placeholder="Enter street address"
+                Name="street"
+                Value={newAddress.street}
+                onChange={handleAddressInputChange}
+              />
+              <InputBox
+                LabelName="City"
+                Placeholder="Enter city"
+                Name="city"
+                Value={newAddress.city}
+                onChange={handleAddressInputChange}
+              />
+              <InputBox
+                LabelName="Country"
+                Placeholder="Enter country"
+                Name="country"
+                Value={newAddress.country}
+                onChange={handleAddressInputChange}
+              />
+              <InputBox
+                LabelName="Postal Code"
+                Placeholder="Enter postal code"
+                Name="postalCode"
+                Value={newAddress.postalCode}
+                onChange={handleAddressInputChange}
+              />
+              <InputBox
+                LabelName="State"
+                Placeholder="Enter state"
+                Name="state"
+                Value={newAddress.state}
+                onChange={handleAddressInputChange}
+              />
+              <div className="flex justify-center items-center gap-5">
                 <Button
-                  className={`mt-4 bg-white text-blue-600 hover:bg-green-500 hover:text-black`}
+                  className={`mt-4  hover:bg-green-500 hover:text-black`}
                   type="button"
                   onClick={addAddress}
                   label="Add Address"
                 />
-              </form>
-              {error && <p className="text-red-500">{error}</p>}
-            </div>
-          </div>
-        )}
-        {showModal2 && (
-          <div className=" fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center backdrop-blur-lg">
-            <h1 className="mb-5 text-center">
-              Hello{" "}
-              <span className="text-2xl font-bold ">{user?.[0]?.name}</span> you
-              can edit your account details here
-            </h1>
-
-            <div className="flex justify-center items-center gap-10 lg:w-1/2 w-full  rounded-xl shadow lg:py-10 whiteSoftBG">
-              <div className="lg:w-1/2 w-full">
-                <form
-                  ref={fromRef}
-                  onSubmit={handleEditProfileSubmit}
-                  className="w-full flex flex-col justify-center items-center px-5 py-2"
-                >
-                  <InputBox
-                    LabelName="Name"
-                    Placeholder={user?.[0]?.name}
-                    Name="name"
-                    Value={editProfile.name}
-                    Type="name"
-                    onChange={handleAddressInputChange2}
-                  />
-                  <InputBox
-                    LabelName="Email Address"
-                    Placeholder={user?.[0]?.email}
-                    Name="email"
-                    Value={editProfile.email}
-                    Type="email"
-                    onChange={handleAddressInputChange2}
-                  />
-                  <InputBox
-                    LabelName="Contact Number"
-                    Placeholder={user?.[0]?.phoneNumber}
-                    Name="phoneNumber"
-                    Type="number"
-                    Value={editProfile.phoneNumber}
-                    onChange={handleAddressInputChange2}
-                  />
-                  <InputBox
-                    LabelName="Password"
-                    Placeholder="Password"
-                    Name="password"
-                    Value={editProfile.password}
-                    Type="password"
-                    onChange={handleAddressInputChange2}
-                  />
-                  <div className="flex justify-center items-center gap-5">
-                    <Button
-                      type="button"
-                      onClick={closeModal2}
-                      label="Cancel"
-                      className="mt-4 hover:bg-orange-500 lg:hidden block"
-                    />
-                    <Button
-                      className={`mt-4 hover:bg-green-500 hover:text-black`}
-                      type="submit"
-                      label="Update Profile"
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="flex flex-col gap-5">
                 <Button
+                  className={`mt-4  hover:bg-green-500 hover:text-black`}
                   type="button"
-                  onClick={closeModal2}
+                  onClick={closeModal}
                   label="Cancel"
-                  className="mt-4 hover:bg-orange-500 hidden lg:block"
                 />
               </div>
+            </form>
+            {error && <p className="text-red-500">{error}</p>}
+          </div>
+        </div>
+      )}
+      {/* Edit profile modal */}
+      {showModal2 && (
+        <div className=" fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center backdrop-blur-lg">
+          <h1 className="mb-5 text-center">
+            Hello <span className="text-2xl font-bold ">{user?.[0]?.name}</span>{" "}
+            you can edit your account details here
+          </h1>
+
+          <div className="flex justify-center items-center gap-10 lg:w-1/2 w-full  rounded-xl shadow lg:py-10 whiteSoftBG">
+            <div className="lg:w-1/2 w-full">
+              <form
+                ref={fromRef}
+                onSubmit={handleEditProfileSubmit}
+                className="w-full flex flex-col justify-center items-center px-5 py-2"
+              >
+                <InputBox
+                  LabelName="Name"
+                  Placeholder={user?.[0]?.name}
+                  Name="name"
+                  Value={editProfile.name}
+                  Type="name"
+                  onChange={handleAddressInputChange2}
+                />
+                <InputBox
+                  LabelName="Email Address"
+                  Placeholder={user?.[0]?.email}
+                  Name="email"
+                  Value={editProfile.email}
+                  Type="email"
+                  onChange={handleAddressInputChange2}
+                />
+                <InputBox
+                  LabelName="Contact Number"
+                  Placeholder={user?.[0]?.phoneNumber}
+                  Name="phoneNumber"
+                  Type="number"
+                  Value={editProfile.phoneNumber}
+                  onChange={handleAddressInputChange2}
+                />
+                <InputBox
+                  LabelName="Password"
+                  Placeholder="Password"
+                  Name="password"
+                  Value={editProfile.password}
+                  Type="password"
+                  onChange={handleAddressInputChange2}
+                />
+                <div className="flex justify-center items-center gap-5">
+                  <Button
+                    type="button"
+                    onClick={closeModal2}
+                    label="Cancel"
+                    className="mt-4 hover:bg-orange-500 lg:hidden block"
+                  />
+                  <Button
+                    className={`mt-4 hover:bg-green-500 hover:text-black`}
+                    type="submit"
+                    label="Update Profile"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="flex flex-col gap-5">
+              <Button
+                type="button"
+                onClick={closeModal2}
+                label="Cancel"
+                className="mt-4 hover:bg-orange-500 hidden lg:block"
+              />
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </section>
   );
 };
