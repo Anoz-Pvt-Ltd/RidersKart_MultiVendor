0ad6093ac80d42abfd521de4849c0b87f87a13d3
Vivek Krishan <132426587+vivek-krishan@users.noreply.github.com>
2025-05-24 12:07:22 +0530

    feat: Add verification status to driver registration and update dotenv version

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -9,6 +9,8 @@ import LoadingUI from "../../Components/Loading";
 import { ThreeProductGrid } from "../../Components/Product-Grid";
 import { truncateString } from "../../Utility/Utility-functions";
 import { useSelector } from "react-redux";
+import Button from "../../Components/Button";
+import axios from "axios";
 
 const Home = ({ startLoading, stopLoading }) => {
   const scrollContainer = useRef(null);
@@ -104,9 +106,9 @@ const Home = ({ startLoading, stopLoading }) => {
     }, []);
 
     return (
-      <div className="relative w-full lg:h-96 h-64 mx-auto overflow-hidden rounded-lg">
+      <div className='relative w-full lg:h-96 h-64 mx-auto overflow-hidden rounded-lg'>
         <div
-          className=" sm:h-60 w-screen h-[44%] transition-transform duration-500 ease-in-out flex object-center  "
+          className=' sm:h-60 w-screen h-[44%] transition-transform duration-500 ease-in-out flex object-center  '
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
           {console.log(promotions)}
@@ -118,20 +120,20 @@ const Home = ({ startLoading, stopLoading }) => {
                   key={promo?._id}
                   src={promo?.images?.url.lg}
                   alt={`Slide ${promo?._id}`}
-                  className="min-w-full min-h-[44vh] "
+                  className='min-w-full min-h-[44vh] '
                 />
               );
             })}
         </div>
         <button
           onClick={prevImage}
-          className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          className='absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full'
         >
           &#8249;
         </button>
         <button
           onClick={nextImage}
-          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          className='absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full'
         >
           &#8250;
         </button>
@@ -139,27 +141,40 @@ const Home = ({ startLoading, stopLoading }) => {
     );
   };
 
+  const handlePrint = async () => {
+    try {
+      const response = await axios.get(
+        `https://lounge.taggteleservices.com/api/v2/SenderId?ApiKey=Uz2+HcIQqS/rEFyvwEo7ZB7wgM1694JE64Tq9VIEbsk=&ClientId=0ea490f7-bfc4-4c50-beb9-e262ab427ca4`
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error printing:", error);
+    }
+  };
+
   return (
-    <div className="container mx-auto py-4">
-      <div className="lg:w-fit flex gap-10 p-5 lg:m-auto  w-full  overflow-x-auto flex-nowrap whitespace-nowrap no-scrollbar">
+    <div className='container mx-auto py-4'>
+      <div className='lg:w-fit flex gap-10 p-5 lg:m-auto  w-full  overflow-x-auto flex-nowrap whitespace-nowrap no-scrollbar'>
         {FamousSubcategory.map((subcategory) => (
           <div
             key={subcategory._id}
-            className="w-24 flex flex-col justify-center items-center  "
+            className='w-24 flex flex-col justify-center items-center  '
           >
-            <div className="w-16 h-16 overflow-hidden rounded-full drop-shadow-xl border-4 border-gray-100">
+            <div className='w-16 h-16 overflow-hidden rounded-full drop-shadow-xl border-4 border-gray-100'>
               <img
                 src={subcategory.image.url}
                 alt={subcategory.title}
-                className="w-full h-full "
+                className='w-full h-full '
               />
             </div>
-            <span className="text-sm">
+            <span className='text-sm'>
               {truncateString(subcategory.title, 10)}
             </span>
           </div>
         ))}
       </div>
+
+      <Button onClick={handlePrint} label={"print"} />
 
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
@@ -170,18 +185,18 @@ const Home = ({ startLoading, stopLoading }) => {
       </motion.div>
 
       {error && (
-        <div className="bg-red-500 text-white p-4 rounded">{error}</div>
+        <div className='bg-red-500 text-white p-4 rounded'>{error}</div>
       )}
 
-      <div className="lg:h-[35vw] h-fit w-full p-2 lg:p-5 my-10 lg:my-0 flex flex-col lg:flex-row gap-10 lg:overflow-x-auto flex-nowrap whitespace-nowrap  scroll-smooth no-scrollbar">
+      <div className='lg:h-[35vw] h-fit w-full p-2 lg:p-5 my-10 lg:my-0 flex flex-col lg:flex-row gap-10 lg:overflow-x-auto flex-nowrap whitespace-nowrap  scroll-smooth no-scrollbar'>
         {arrayOfGridItems.map((item, index) => (
-          <div key={index} className="flex-none w-fit">
+          <div key={index} className='flex-none w-fit'>
             <ThreeProductGrid products={item.products} heading={item.title} />
           </div>
         ))}
       </div>
 
-      <div className="mx-10">
+      <div className='mx-10'>
         {categories.map((category, index) => {
           const scrollLeft = () => {
             if (scrollContainer.current) {
@@ -204,24 +219,24 @@ const Home = ({ startLoading, stopLoading }) => {
           return (
             <motion.section
               key={category._id} // Use unique id for key
-              className="mb-12"
+              className='mb-12'
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.2 }}
             >
               {/* Category Title */}
-              <div className="flex justify-between items-center">
-                <h2 className="text-lg font-bold">{category.title}</h2>
-                <div className="flex gap-2">
+              <div className='flex justify-between items-center'>
+                <h2 className='text-lg font-bold'>{category.title}</h2>
+                <div className='flex gap-2'>
                   <button
                     onClick={scrollLeft}
-                    className="headerBg text-white px-3 py-1 rounded shadow hover:bg-purple-800"
+                    className='headerBg text-white px-3 py-1 rounded shadow hover:bg-purple-800'
                   >
                     &#8249;
                   </button>
                   <button
                     onClick={scrollRight}
-                    className="headerBg text-white px-3 py-1 rounded shadow hover:bg-purple-800"
+                    className='headerBg text-white px-3 py-1 rounded shadow hover:bg-purple-800'
                   >
                     &#8250;
                   </button>
@@ -231,7 +246,7 @@ const Home = ({ startLoading, stopLoading }) => {
               {/* Subcategories */}
               <div
                 ref={scrollContainer}
-                className="flex overflow-x-auto overflow-y-hidden scrollbar-hide gap-4 py-2 px-1 justify-start items-start "
+                className='flex overflow-x-auto overflow-y-hidden scrollbar-hide gap-4 py-2 px-1 justify-start items-start '
               >
                 {category.subcategories.length > 0 ? (
                   category.subcategories.map((item) => (
@@ -240,25 +255,25 @@ const Home = ({ startLoading, stopLoading }) => {
                       key={item._id}
                     >
                       <motion.div
-                        className="flex-none border border-gray-200 bg-white rounded-lg p-2 text-center shadow-md hover:shadow-lg w-48 h-fit "
+                        className='flex-none border border-gray-200 bg-white rounded-lg p-2 text-center shadow-md hover:shadow-lg w-48 h-fit '
                         whileHover={{ scale: 1.05 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         initial={{ opacity: 0, x: -100 }}
                         transition={{ duration: 0.5 }}
                       >
-                        <div className="object-fill h-24 bg-white rounded-md mb-2 flex justify-center items-center p-10 overflow-hidden">
+                        <div className='object-fill h-24 bg-white rounded-md mb-2 flex justify-center items-center p-10 overflow-hidden'>
                           <img
                             src={item.image?.url}
                             alt={item.title}
-                            className="object-fill"
+                            className='object-fill'
                           />
                         </div>
-                        <p className="text-sm ">{item.title}</p>
+                        <p className='text-sm '>{item.title}</p>
                       </motion.div>
                     </Link>
                   ))
                 ) : (
-                  <p className="text-gray-500">No subcategories available</p>
+                  <p className='text-gray-500'>No subcategories available</p>
                 )}
               </div>
             </motion.section>
@@ -266,7 +281,7 @@ const Home = ({ startLoading, stopLoading }) => {
         })}
       </div>
 
-      <div className="flex gap-4 bg-transparent justify-start items-center overflow-x-auto p-5 max-w-full no-scrollbar">
+      <div className='flex gap-4 bg-transparent justify-start items-center overflow-x-auto p-5 max-w-full no-scrollbar'>
         {products?.map((product) => (
           <ProductCard
             Image={product?.images[0]?.url}
