8054e749716eb8c55313cae8db8005c0f588494f
Vivek Krishan <132426587+vivek-krishan@users.noreply.github.com>
2025-08-13 12:55:27 +0530

    feat: Add refresh button and improve fetchProducts function in ManageStocks component

--- VendorAdmin/src/pages/Manage_Stocks/ManageStocks.jsx
+++ VendorAdmin/src/pages/Manage_Stocks/ManageStocks.jsx
@@ -3,7 +3,7 @@ import { FetchData } from "../../utils/FetchFromApi";
 import { useSelector } from "react-redux";
 import LoadingUI from "../../components/Loading";
 import Button from "../../components/Button";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, RotateCw } from "lucide-react";
 import InputBox from "../../components/InputBox";
 
 const ManageStocks = ({ startLoading, stopLoading }) => {
@@ -22,26 +22,27 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
     product?.name?.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const fetchProducts = async () => {
+    try {
+      startLoading();
+      const response = await FetchData(
+        `products/get-all-product-of-vendor/${user?.[0]?._id}`,
+        "get"
+      );
+      // console.log(response);
+      if (response.data.success) setProducts(response.data.data);
+    } catch (err) {
+      setError(err.response?.data?.message || "Failed to fetch products.");
+    } finally {
+      stopLoading();
+    }
+  };
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        startLoading();
-        const response = await FetchData(
-          `products/get-all-product-of-vendor/${user?.[0]?._id}`,
-          "get"
-        );
-        // console.log(response);
-        if (response.data.success) setProducts(response.data.data);
-      } catch (err) {
-        setError(err.response?.data?.message || "Failed to fetch products.");
-      } finally {
-        stopLoading();
-      }
-    };
     fetchProducts();
   }, []);
   // console.log(products);
-  const handleAddStock = async () => {
+  const handleAddStock = async (e) => {
+    e.preventDefault();
     const formData = new FormData(formRef.current);
     for (let [key, value] of formData.entries()) {
       console.log(`${key}: ${value}`);
@@ -59,10 +60,15 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
       alert("stock not added");
       setError(err.response?.data?.message || "Failed to add stock.");
     } finally {
+      setHandlePopup((prev) => {
+        return { ...prev, addStocksPopup: false };
+      });
       stopLoading();
     }
   };
-  const handleRemoveStock = async () => {
+  const handleRemoveStock = async (e) => {
+    e.preventDefault();
+
     const formData = new FormData(formRef.current);
     for (let [key, value] of formData.entries()) {
       console.log(`${key}: ${value}`);
@@ -80,59 +86,67 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
       alert("stock not added");
       setError(err.response?.data?.message || "Failed to remove stock.");
     } finally {
+      setHandlePopup((prev) => {
+        return { ...prev, addStocksPopup: false };
+      });
       stopLoading();
     }
   };
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg">
-      <h2 className="text-2xl font-bold text-gray-700 mb-4">Manage Stocks</h2>
+    <div className='bg-white p-6 rounded-lg shadow-lg'>
+      <div className="flex gap-5 items-center mb-5  ">
+        <h2 className='text-2xl font-bold text-gray-700 '>Manage Stocks</h2>
+        <button onClick={fetchProducts} className=''>
+          <RotateCw size={20} color='green' />
+        </button>
+      </div>
       <p>Monitor and manage stock levels here.</p>
-      <div className="mb-4">
+      <div className='mb-4'>
         <input
-          type="text"
-          placeholder="Search by Product name to modify stocks..."
+          type='text'
+          placeholder='Search by Product name to modify stocks...'
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200 ease-in-out hover:shadow-md"
+          className='w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 outline-none transition duration-200 ease-in-out hover:shadow-md'
         />
       </div>
 
-      <div className="overflow-x-auto mt-5">
-        <table className="min-w-full bg-white border border-gray-200 rounded-lg">
+      <div className='overflow-x-auto mt-5'>
+        <table className='min-w-full bg-white border border-gray-200 rounded-lg'>
           <thead>
-            <tr className="bg-gray-100 text-gray-600 text-left text-sm font-medium">
-              <th className="py-3 px-4 border-b">Product ID</th>
-              <th className="py-3 px-4 border-b">Category Name</th>
-              <th className="py-3 px-4 border-b">Subcategory Name</th>
-              <th className="py-3 px-4 border-b">Product Name</th>
-              <th className="py-3 px-4 border-b">Stock Quantity</th>
-              <th className="py-3 px-4 border-b text-center">Activity</th>
+            <tr className='bg-gray-100 text-gray-600 text-left text-sm font-medium'>
+              <th className='py-3 px-4 border-b'>Product ID</th>
+              <th className='py-3 px-4 border-b'>Category Name</th>
+              <th className='py-3 px-4 border-b'>Subcategory Name</th>
+              <th className='py-3 px-4 border-b'>Product Name</th>
+              <th className='py-3 px-4 border-b'>Stock Quantity</th>
+              <th className='py-3 px-4 border-b text-center'>Activity</th>
             </tr>
           </thead>
           <tbody>
             {filteredProducts.map((elements) => (
               <tr
                 key={elements._id}
-                className="text-sm text-gray-700 border-b hover:bg-gray-50"
+                className='text-sm text-gray-700 border-b hover:bg-gray-50'
               >
-                <td className="py-3 px-4">{elements?._id}</td>
-                <td className="py-3 px-4 font-semibold">
+                <td className='py-3 px-4'>{elements?._id}</td>
+                <td className='py-3 px-4 font-semibold'>
                   {elements?.category?.title}
                 </td>
-                <td className="py-3 px-4 font-semibold">
+                <td className='py-3 px-4 font-semibold'>
                   {elements?.subcategory?.title}
                 </td>
-                <td className="py-3 px-4 font-semibold">{elements?.name}</td>
-                <td className="py-3 px-4 text-center">
+                <td className='py-3 px-4 font-semibold'>{elements?.name}</td>
+                <td className='py-3 px-4 text-center'>
                   {elements?.stockQuantity}
                 </td>
                 <td
                   className={`py-3 px-4 font-bold flex justify-center items-center`}
                 >
-                  <div className="flex gap-5">
+                  <div className='flex gap-5'>
                     <button
-                      className="text-green-500 flex justify-center items-center text-xs"
+                      className='text-green-500 flex justify-center items-center text-xs'
                       // onClick={() => {
                       //   setProductId(elements._id);
                       //   // handleAddStock();
@@ -154,7 +168,7 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
                           return { ...prev, removeStocksPopup: true };
                         });
                       }}
-                      className="text-red-500 flex justify-center items-center text-xs"
+                      className='text-red-500 flex justify-center items-center text-xs'
                     >
                       <Minus />
                       Remove
@@ -167,11 +181,11 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
         </table>
       </div>
       {handlePopup.addStocksPopup && (
-        <div className="absolute top-0 left-0 backdrop-blur-2xl h-screen w-screen flex justify-center items-center">
+        <div className='absolute top-0 left-0 backdrop-blur-2xl h-screen w-screen flex justify-center items-center'>
           <form
             ref={formRef}
             onSubmit={handleAddStock}
-            className="w-96 h-fit bg-white px-5 py-5 shadow-2xl rounded-xl "
+            className='w-96 h-fit bg-white px-5 py-5 shadow-2xl rounded-xl '
           >
             <h1>Adding stock on product id: {productId}</h1>
             <InputBox
@@ -179,10 +193,10 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
               Placeholder={"Not less than 1"}
               Name={"quantityToAdd"}
               // Value={"addStock"}
-              Type="number"
+              Type='number'
               Required
             />
-            <div className="flex flex-col gap-2">
+            <div className='flex flex-col gap-2'>
               <Button
                 label={"Add Stock"}
                 type={"submit"}
@@ -202,11 +216,11 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
         </div>
       )}
       {handlePopup.removeStocksPopup && (
-        <div className="absolute top-0 left-0 backdrop-blur-2xl h-screen w-screen flex justify-center items-center">
+        <div className='absolute top-0 left-0 backdrop-blur-2xl h-screen w-screen flex justify-center items-center'>
           <form
             ref={formRef}
             onSubmit={handleRemoveStock}
-            className="w-96 h-fit bg-white px-5 py-5 shadow-2xl rounded-xl "
+            className='w-96 h-fit bg-white px-5 py-5 shadow-2xl rounded-xl '
           >
             <h1>Removing stock on product id: {productId}</h1>
             <InputBox
@@ -214,10 +228,10 @@ const ManageStocks = ({ startLoading, stopLoading }) => {
               Placeholder={"Not less than 1"}
               Name={"quantityToRemove"}
               // Value={"RemoveStock"}
-              Type="number"
+              Type='number'
               Required
             />
-            <div className="flex flex-col gap-2">
+            <div className='flex flex-col gap-2'>
               <Button
                 label={"Remove Stock"}
                 type={"submit"}
