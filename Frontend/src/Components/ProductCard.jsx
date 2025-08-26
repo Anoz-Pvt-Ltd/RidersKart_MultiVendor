cfb76ccc5125d52df02a5a619cfc2970415acad5
Vivek Krishan <132426587+vivek-krishan@users.noreply.github.com>
2025-08-26 15:44:34 +0530

    mistake solved

--- Frontend/src/Components/ProductCard.jsx
+++ Frontend/src/Components/ProductCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
-import React from "react";
 import { FetchData } from "../Utility/FetchFromApi";
 import { addCart } from "../Utility/Slice/CartSlice";
 
