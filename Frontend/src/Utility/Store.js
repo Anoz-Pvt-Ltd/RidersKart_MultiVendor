6975c92f8fd89a0f9827ef5161c6bbc3ce1f3f89
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-09-14 15:00:32 +0530

    feat: add RatingSlice to the Redux store for product ratings management

--- Frontend/src/Utility/Store.js
+++ Frontend/src/Utility/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import UserInfoSlice from "./Slice/UserInfoSlice";
 import CartList from "./Slice/CartSlice";
 import PromotionList from "./Slice/PromotionsSlice";
+import RatingSlice from "./Slice/RatingSlice";
 
 const store = configureStore({
   reducer: {
     UserInfo: UserInfoSlice,
     CartList: CartList,
     PromotionList: PromotionList,
+    Rating: RatingSlice,
   },
 });
 
