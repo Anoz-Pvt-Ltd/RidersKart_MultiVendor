7b6afc7d2599a398a82ba9c717132932fd5ff041
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-11-29 23:27:04 +0530

    Update selection background color in App component for improved accessibility

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
   }, [dispatch, user]);
 
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] overflow-x-hidden antialiased selection:bg-cyan-500 selection:text-cyan-900">
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] overflow-x-hidden antialiased selection:bg-neutral-500 selection:text-white">
       <div className="text-black">
         <Header />
         <Routes>
