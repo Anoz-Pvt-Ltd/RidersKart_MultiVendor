0448379f8821488d0516084f46722e152d84646d
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-12-02 11:47:17 +0530

    Remove commented console.log statement in VerifyAdminUser for cleaner code

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ const VerifyAdminUser = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    // console.log(token);
+    console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
